ff833ad1a81005e0b6a31801680526d527d36434
hjonsu <jsu6898@gmail.com>
2023-12-17 02:09:55 -0800

    solution 2

--- LeetCode/searchA2dArray.js
+++ LeetCode/searchA2dArray.js
@@ -4,43 +4,68 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-  let thisArr = [];
-  if (matrix.length === 1) {
-    thisArr = matrix[0];
-  } else {
-    x = 1;
-    while (x < matrix.length) {
-      if (target === matrix[x][0]) {
-        return true;
-      }
-      if (target < matrix[x][0]) {
-        thisArr = matrix[x - 1];
-        break;
-      }
-      if (x === matrix.length - 1) {
-        thisArr = matrix[x];
-        break;
-      }
-      x++;
-    }
+  if (!matrix || matrix.length === 0 || matrix[0].length === 0) {
+    return false;
   }
-  let l = 0;
-  let r = thisArr.length - 1;
 
-  while (l <= r) {
-    let mid = Math.floor((l + r) / 2);
-    if (target === thisArr[mid]) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  let left = 0;
+  let right = rows * cols - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midValue = matrix[Math.floor(mid / cols)][mid % cols];
+
+    if (target === midValue) {
       return true;
-    }
-    if (target < thisArr[mid]) {
-      r = mid;
-    }
-    if (target > thisArr[mid]) {
-      l = mid + 1;
-    }
-    if (l === r && target !== thisArr[l]) {
-      return false;
+    } else if (target < midValue) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
     }
   }
+
   return false;
+
+  //   let thisArr = [];
+  //   if (matrix.length === 1) {
+  //     thisArr = matrix[0];
+  //   } else {
+  //     x = 1;
+  //     while (x < matrix.length) {
+  //       if (target === matrix[x][0]) {
+  //         return true;
+  //       }
+  //       if (target < matrix[x][0]) {
+  //         thisArr = matrix[x - 1];
+  //         break;
+  //       }
+  //       if (x === matrix.length - 1) {
+  //         thisArr = matrix[x];
+  //         break;
+  //       }
+  //       x++;
+  //     }
+  //   }
+  //   let l = 0;
+  //   let r = thisArr.length - 1;
+
+  //   while (l <= r) {
+  //     let mid = Math.floor((l + r) / 2);
+  //     if (target === thisArr[mid]) {
+  //       return true;
+  //     }
+  //     if (target < thisArr[mid]) {
+  //       r = mid;
+  //     }
+  //     if (target > thisArr[mid]) {
+  //       l = mid + 1;
+  //     }
+  //     if (l === r && target !== thisArr[l]) {
+  //       return false;
+  //     }
+  //   }
+  //   return false;
 };
