be7fc498d5e7770494edba45fb6f9fe057260dda
hjonsu <jsu6898@gmail.com>
2023-12-19 09:18:24 -0800

    refactor solution to 1TwoSum.js

--- LeetCode/1TwoSum.js
+++ LeetCode/1TwoSum.js
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/two-sum/submissions/1123558682
 /**
  * Attempt 1
  * @param {number[]} nums
@@ -14,5 +15,25 @@ var twoSum = function (nums, target) {
         return [i, k];
       }
     }
+  }
+};
+
+// much faster solution
+// https://leetcode.com/problems/two-sum/submissions/1123577059
+
+/**
+ * Attempt 2
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    const number = target - nums[i];
+    if (obj[number]) {
+      return [obj[number] - 1, i];
+    }
+    obj[nums[i]] = i + 1;
   }
 };
